d3fb05215a646bebebc1db5c58742f00eb10f2c0
Katsumouley <vitorluizsantosmougenot@gmail.com>
2025-07-29 20:36:45 -0300

    adding up new stuff

--- app/src/main/java/com/example/practiceaplication/examples/class2907.kt
+++ app/src/main/java/com/example/practiceaplication/examples/class2907.kt
@@ -5,9 +5,20 @@ fun main()
     var nome = "Katsu"
     val constant = 10
 
-    println("hello world\nI'm alive\nI'm Happy$constant $nome")
+    println("hello world\nI'm alive\nI'm Happy $constant $nome")
     nome = "vitor"
     println(nome)
+    println("\n-------------------\nvamos somar os valores 1 e 2\nA soma desses valores é "+ somar(1,2))
+    val frutas = mutableListOf("Banana","Maçã","Morango")
+    println("\n-------------------\n")
+    println("O array frutas é igual a "+frutas)
+    frutas.add("Tomate")
+    println("Após a adição o array frutas é igual a "+frutas)
+    frutas.removeAt(3)
+    frutas.removeAt(2)
+    println("Após as 2 remoções o array frutas é igual a "+frutas)
+
+    println("\n-------------------\n")
 }
 
-fun somar(){}
+fun somar(value1: Int, value2: Int): Int = value1 + value2
